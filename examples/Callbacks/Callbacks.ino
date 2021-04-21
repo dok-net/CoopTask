06ef680f2fb2f7739a0d52381fd4f2a6512f9fbf
Dirk O. Kaar <dok@dok-net.net>
2021-04-21 18:21:11 +0200

    Add synthetic callback example to show how callbacks get invoked from runCoopTasks depending on tasks' running states.

---
+++ examples/Callbacks/Callbacks.ino
@@ -0,0 +1,85 @@
+#include <CoopTaskBase.h>
+#include <CoopTask.h>
+#include <CoopSemaphore.h>
+#include <CoopMutex.h>
+#include <BasicCoopTask.h>
+
+CoopTask<void>* blinkTask = nullptr;
+CoopTask<void>* switchTask = nullptr;
+
+void blinkFunction()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    for (;;)
+    {
+        yield(); // A
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(5000); // B
+        digitalWrite(LED_BUILTIN, HIGH);
+        CoopTask<void>::sleep(); // D
+    }
+}
+
+void switchFunction()
+{
+    for (;;)
+    {
+        yield(); // A
+        Serial.println(F("Switch on"));
+        delay(100); // B
+        Serial.println(F("Switch off"));
+        CoopTask<void>::sleep(); // C
+    }
+}
+
+bool delayCb(uint32_t ms)
+{
+    Serial.print(F("delayDb, ms = "));
+    Serial.println(ms);
+    delay(ms);
+    return true;
+}
+
+bool sleepCb()
+{
+    Serial.println(F("sleepCb"));
+    Serial.flush();
+    delay(10000);
+    if (blinkTask) blinkTask->wakeup();
+    if (switchTask) switchTask->wakeup();
+    return true;
+}
+
+void setup()
+{
+    Serial.begin(74800);
+    while (!Serial);
+    delay(100);
+    Serial.println();
+    Serial.println(F("runTasks callback test"));
+
+    runCoopTasks(nullptr, delayCb, sleepCb);
+    Serial.println(F("no tasks yet, sleepCb()?"));
+
+    blinkTask = new CoopTask<void>(F("blink"), blinkFunction);
+    switchTask = new CoopTask<void>(F("switch"), switchFunction);
+    blinkTask->scheduleTask();
+    switchTask->scheduleTask();
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+    runCoopTasks(nullptr, delayCb, sleepCb);
+    Serial.println(F("A - both tasks yielded, no Cb?"));
+    yield();
+    runCoopTasks(nullptr, delayCb, sleepCb);
+    Serial.println(F("B - both tasks delayed, delayCb(100)?"));
+    yield();
+    runCoopTasks(nullptr, delayCb, sleepCb);
+    Serial.println(F("C - blink task delayed, buzzer task sleeping, delayCb(4900)?"));
+    yield();
+    runCoopTasks(nullptr, delayCb, sleepCb);
+    Serial.println(F("D - both tasks sleeping, sleepCb()?"));
+}
